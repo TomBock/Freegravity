3ef218187bf6b0b7bc7b4278cbec3e8ae9257165
Tom Bockhorn <bockhorn.tom@web.de>
2018-12-02 21:27:10 +0100

    Initial commit

---
+++ src/main/java/de/cybiduck/freegravity/Freegravity.java
@@ -0,0 +1,17 @@
+package de.cybiduck.freegravity;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Freegravity extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
